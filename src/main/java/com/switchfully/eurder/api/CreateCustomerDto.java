fe262f1b8e4f5eed4f68dc882b00e88a5e233e3d
tom-scheyltjens <tom_scheyltjens@outlook.com>
2021-12-06 15:36:43 +0100

    (story 1 - Create a customer account) - all address fields required via notnull and valid annotations

--- src/main/java/com/switchfully/eurder/api/CreateCustomerDto.java
+++ src/main/java/com/switchfully/eurder/api/CreateCustomerDto.java
@@ -2,12 +2,13 @@ package com.switchfully.eurder.api;
 
 import com.switchfully.eurder.domain.Address;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public record CreateCustomerDto(@NotEmpty(message = "first name should be provided") String firstName,
                                 @NotEmpty(message = "last name should be provided") String lastName,
-                                @NotNull(message = "address should be provided") Address address,
+                                @NotNull(message = "address should be provided") @Valid Address address,
                                 @NotEmpty(message = "email address  should be provided")String emailAddress,
                                 @NotEmpty(message = "phone number should be provided") String phoneNumber) {
 }
--- src/main/java/com/switchfully/eurder/domain/Address.java
+++ src/main/java/com/switchfully/eurder/domain/Address.java
@@ -1,14 +1,10 @@
 package com.switchfully.eurder.domain;
 
-public record Address(String streetName, String houseNumber, String postalCode, String city) {
+import javax.validation.constraints.NotEmpty;
 
-    @Override
-    public String toString() {
-        return "Address{" +
-                "streetName='" + streetName + '\'' +
-                ", houseNumber='" + houseNumber + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", city='" + city + '\'' +
-                '}';
-    }
+public record Address(@NotEmpty(message = "street name should be provided")String streetName,
+                      @NotEmpty(message = "house number should be provided")String houseNumber,
+                      @NotEmpty(message = "postal code should be provided")String postalCode,
+                      @NotEmpty(message = "city should be provided")String city) {
+
 }
