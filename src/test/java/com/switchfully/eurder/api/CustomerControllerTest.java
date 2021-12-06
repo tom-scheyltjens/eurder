c580f435ef8ea6c2d847915033175ce5cbdfbd58
tom-scheyltjens <tom_scheyltjens@outlook.com>
2021-12-06 15:40:28 +0100

    (story 1 - Create a customer account) - created integration test for non-happy path post customers

--- src/test/java/com/switchfully/eurder/api/CustomerControllerTest.java
+++ src/test/java/com/switchfully/eurder/api/CustomerControllerTest.java
@@ -1,11 +1,8 @@
 package com.switchfully.eurder.api;
 
 import com.switchfully.eurder.domain.Address;
-import com.switchfully.eurder.repository.CustomerRepository;
-import com.switchfully.eurder.service.CustomerService;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -17,14 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CustomerControllerTest {
     @Value("${server.port}")
     private int port;
-    private CustomerRepository customerRepository;
-    private CustomerService customerService;
-
-    @Autowired
-    CustomerControllerTest(CustomerRepository customerRepository, CustomerService customerService) {
-        this.customerRepository = customerRepository;
-        this.customerService = customerService;
-    }
 
     @Test
     void createCustomer_givenACustomerToCreate_thenTheNewlyCreatedCustomerIsSavedAndReturned() {
@@ -51,5 +40,23 @@ public class CustomerControllerTest {
         assertThat(customerDto.address()).isEqualTo(createCustomerDto.address());
         assertThat(customerDto.emailAddress()).isEqualTo(createCustomerDto.emailAddress());
         assertThat(customerDto.phoneNumber()).isEqualTo(createCustomerDto.phoneNumber());
+    }
+
+    @Test
+    void createCustomer_givenACustomerToCreateWithEmptyFirstName_thenAnErrorMessageIsReturnedWithHttpStatusBadRequest() {
+        CreateCustomerDto createCustomerDto = new CreateCustomerDto("", "Sch", new Address("Teststreet", "14", "2300", "Turnhout"), "tom@sch.com", "0123456789");
+
+
+        RestAssured
+                .given()
+                .body(createCustomerDto)
+                .accept(JSON)
+                .contentType(JSON)
+                .when()
+                .port(port)
+                .post("/customers")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
