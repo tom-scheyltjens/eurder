4920e97d0ffb121df4bdade2f11c38198adf42b1
tom-scheyltjens <tom_scheyltjens@outlook.com>
2021-12-07 15:20:14 +0100

    (story 2) - added checks to see if all fields in item are filled in and correct

--- src/main/java/com/switchfully/eurder/api/item/CreateItemDto.java
+++ src/main/java/com/switchfully/eurder/api/item/CreateItemDto.java
@@ -1,4 +1,11 @@
 package com.switchfully.eurder.api.item;
 
-public record CreateItemDto(String name, String description, double price, int amount) {
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public record CreateItemDto(@NotEmpty(message = "name should be provided") String name,
+                            @NotEmpty(message = "description should be provided") String description,
+                            @Min(value = 0, message = "price should be greater then 0") @NotNull(message = "price should be provided") Double price,
+                            @Min(value = 1, message = "amount should be bigger than 0") @NotNull(message = "amount should be provided") Integer amount) {
 }
--- src/main/java/com/switchfully/eurder/api/item/ItemController.java
+++ src/main/java/com/switchfully/eurder/api/item/ItemController.java
@@ -1,10 +1,19 @@
 package com.switchfully.eurder.api.item;
 
+import com.switchfully.eurder.api.customer.CustomerController;
 import com.switchfully.eurder.domain.Item;
 import com.switchfully.eurder.service.ItemService;
 import com.switchfully.eurder.service.SecurityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.switchfully.eurder.domain.user.Feature.CREATE_ITEM;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -15,6 +24,7 @@ public class ItemController {
     private final ItemService itemService;
     private final ItemMapper itemMapper;
     private final SecurityService securityService;
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
 
     public ItemController(ItemService itemService, ItemMapper itemMapper, SecurityService securityService) {
         this.itemService = itemService;
@@ -24,10 +34,24 @@ public class ItemController {
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public ItemDto createItem(@RequestHeader String authorization, @RequestBody CreateItemDto createItemDto) {
+    public ItemDto createItem(@RequestHeader String authorization,@Valid @RequestBody CreateItemDto createItemDto) {
         securityService.validate(authorization, CREATE_ITEM);
         Item item = itemMapper.createItemToItem(createItemDto);
         itemService.addItem(item);
         return itemMapper.itemToItemDto(item);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+            LOGGER.error(errorMessage, exception);
+        });
+        return errors;
     }
 }
